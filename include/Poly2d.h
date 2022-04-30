425c38134405e9f84c3287ce4de18924bd17f5de
andrea <andreamaria.zanchettin@polimi.it>
2022-04-30 19:08:01 +0200

    routine to order vertices counter-clockwise

--- include/Poly2d.h
+++ include/Poly2d.h
@@ -7,14 +7,15 @@
 class Poly2d {
 public:
 // constructor
-    Poly2d(std::vector<Eigen::Vector2d>& points): mPoints(points) { };
+    Poly2d(std::vector<Eigen::Vector2d>& points);
 // methods
     Poly2d ConvexUnion(Poly2d &p);
     Poly2d MinkowskiSum(Poly2d &p);
-    Poly2d Intersection(Poly2d &p);
+    Poly2d MinkowskiDiff(Poly2d &p);
 private:
 // convex hull
     void Simplify();
+    void OrderVertices();
 // data
     std::vector<Eigen::Vector2d> mPoints;
 };
--- src/Poly2d.cpp
+++ src/Poly2d.cpp
@@ -1,8 +1,18 @@
 #include "Poly2d.h"
 #include "QuickHull.hpp"
+#include <pair>
 
 using namespace std;
 using namespace Eigen;
+
+bool sort_fun(vector<pair<Vector2d, double> >& x, vector<pair<Vector2d, double> >& y) {
+    return (x.second > y.second);
+}
+
+Poly2d::Poly2d(std::vector<Eigen::Vector2d>& points) {
+    this->mPoints = points;
+    this->Simplify();
+}
 
 Poly2d Poly2d::ConvexUnion(Poly2d &p) {
     
@@ -34,6 +44,24 @@ Poly2d Poly2d::MinkowskiSum(Poly2d &p) {
     
 }
 
+Poly2d Poly2d::MinkowskiDiff(Poly2d &p) {
+    
+    // computing Minkowski sum
+    vector<Vector2d> v;
+    vector<Vector2d>::iterator it1, it2;
+    for(it1 = this->mPoints.begin(); it1 != this->mPoints.end(); ++it1) {
+        for(it2 = p.mPoints.begin(); it2 != p.mPoints.end(); ++it2) {
+            v.push_back(*it1 - *it2);
+        }
+    }
+    
+    // simplify and return
+    Poly2d m = Poly2d(v); m.Simplify();
+    return m;
+    
+}
+
+
 void Poly2d::Simplify() {
     
     // from Eigen to Quickhull
@@ -47,19 +75,32 @@ void Poly2d::Simplify() {
     const auto& indexBuffer = hull.getIndexBuffer();
     const auto& vertexBuffer = hull.getVertexBuffer();
     
-    // Extract vertices
+    // Extract vertices and order them counter-clockwise
     vector<Vector2d> vsimple;
     for(size_t i = 0; i < indexBuffer.size(); ++i)
         vsimple.push_back( Vector2d(vertexBuffer[indexBuffer[i]].x, vertexBuffer[indexBuffer[i]].y) );
     this->mPoints = vsimple;
+    this->OrderVertices();
     
 }
 
-Poly2d Poly2d::Intersection(Poly2d &p) {
+void Poly2d::OrderVertices() {
     
-    // to be done
-    vector<Vector2d> v;
-    Poly2d m = Poly2d(v); m.Simplify();
-    return m;
+    // compute centroid
+    Vector2d c = Vector2d(.0,.0,.0);
+    vector<Vector2d>::iterator it;
+    for(it = this->mPoints.begin(); it != this->mPoints.end(); ++it) c += *it;
+    c /= double(this->mPoints.size());
+    
+    // order vertices counter-clockwise
+    vector<pair<Vector2d, double> > to_order;
+    for(it = this->mPoints.begin(); it != this->mPoints.end(); ++it)
+        to_order.push_back(std::make_pair(*it, atan2((*it)(1)-c(1), (*it)(0)-c(0))));
+    sort(to_order.begin(), to_order.end(), sort_fun);
+    this->mPoints.clear();
+    
+    // extract vertices
+    vector<pair<Vector2d, double> >::iterator its;
+    for(its = to_order.begin(); its != to_order.end(); ++its) this->mPoints.push_back(its->first);
     
 }
